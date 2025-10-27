8b32a09e7d3fc79880ae13fd66d251228b7987d2
Weslen Oliveira <160598405+Weslen-Oliveira@users.noreply.github.com>
2025-10-26 22:54:44 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        // Configurações do Swiper
+        slidesPerView: 5,
+        centeredSlides: false,
+        spaceBetween: 20,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+            breakpoints: {
+            320: {
+                slidesPerView: 1,
+                loop: true,
+            },
+                // Telas >= 640px
+            640: {
+                slidesPerView: 2,
+            },
+                // Telas >= 1024px
+            1024: {
+                slidesPerView: 3,
+            },
+            1400:{
+                slidesPerView: 4
+            }
+        }
+    });
+});
+
